91a2046e18615b2f72ef1cc27e4e144c0f41d276
TaylorHan <rhaktkdl@gmail.com>
2015-05-24 03:12:31 +0900

    Create New Project
    * Created New Project
    * Modified Note Maker/Move

---
+++ Assets/Scripts/NoteMaker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteMaker : MonoBehaviour {
+	public GameObject[] Notes;
+	GameObject newNote;
+
+	float dist;
+	//Vector4 camBoundary;
+	float boundaryRight;
+
+	void Awake() {
+		dist = (transform.position - Camera.main.transform.position).z;
+
+		//Get Right Boundary
+		//camBoundary.x = Camera.main.ViewportToWorldPoint(new Vector3(1,0,dist)).x;
+		boundaryRight = Camera.main.ViewportToWorldPoint(new Vector3(1,0,dist)).x;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (true == CheckNext())
+			newNote = MakeNote ();
+	}
+
+	bool CheckNext()
+	{
+		if (null != newNote) {
+			float posX = newNote.transform.position.x;
+			float sizeW = newNote.GetComponent<Collider2D>().bounds.size.x;
+			if (posX + sizeW > boundaryRight)
+				return false;
+		}
+
+		return true;
+	}
+
+	GameObject MakeNote()
+	{
+		int randIndex = Random.Range (0, Notes.Length);
+		GameObject note = Instantiate (Notes [randIndex]) as GameObject;
+		float noteSizeW = note.GetComponent<Collider2D>().bounds.size.x;
+		note.transform.position = new Vector3 (boundaryRight + noteSizeW, 0, 0);
+		return note;
+	}
+}
---
+++ Assets/Scripts/NoteMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteMove : MonoBehaviour {
+	public Vector2 speed;
+	public Vector2 direction;
+
+	float dist;
+	float boundaryLeft;
+
+	void Awake()
+	{
+		dist = (transform.position - Camera.main.transform.position).z;
+		boundaryLeft = Camera.main.ViewportToWorldPoint(new Vector3(0,0,dist)).x;
+	}
+	// Use this for initialization
+	void Start () {
+
+		speed = new Vector2 (0.1f, 0);
+		direction = new Vector2 (-1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 curPos = transform.position;
+		float newPosX = curPos.x + speed.x * direction.x;
+		float newPosY = curPos.y + speed.y * direction.y;
+		Vector3 newPos = new Vector3(newPosX, newPosY, curPos.z);
+		transform.position = newPos;
+
+		//Check if it is out of Screen
+		float sizeW = GetComponent<Collider2D>().bounds.size.x; 
+		if (newPosX + sizeW < boundaryLeft)
+			Destroy(gameObject);
+	}
+}
